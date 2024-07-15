6d7ae6c3bf30bc647d20421538a7e0340192b190
Kalyani Bajirao <95849305+kalyanibajirao@users.noreply.github.com>
2024-07-15 22:19:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,6 @@
-//complete this code
 class Animal {
     constructor(species) {
-        this._species = species;
-    }
-
-    get species() {
-        return this._species;
+        this.species = species;
     }
 
     makeSound() {
@@ -24,7 +19,3 @@ class Dog extends Animal {
         console.log("woof");
     }
 }
-// Do not change the code below this line
-window.Animal = Animal;
-window.Dog = Dog;
-window.Cat = Cat;
