3a1eb12d6161729d9934876cd405e1f8f1b1893b
Kalyani Bajirao <95849305+kalyanibajirao@users.noreply.github.com>
2024-07-18 20:41:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,31 +1,35 @@
+// animal.js
 class Animal {
   constructor(species) {
     this._species = species;
   }
 
-  // Getter for species
   get species() {
     return this._species;
   }
 
-  // Method to make a sound
   makeSound() {
     console.log(`The ${this.species} makes a sound`);
   }
 }
 
-// Define the Cat class that extends Animal
 class Cat extends Animal {
-  // Method to purr
   purr() {
     console.log("purr");
   }
 }
 
-// Define the Dog class that extends Animal
 class Dog extends Animal {
-  // Method to bark
   bark() {
     console.log("woof");
   }
+}
+
+// Export the classes if using a module system
+if (typeof module !== 'undefined' && module.exports) {
+  module.exports = { Animal, Cat, Dog };
+} else {
+  window.Animal = Animal;
+  window.Cat = Cat;
+  window.Dog = Dog;
 }
